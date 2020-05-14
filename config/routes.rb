cca87c23adfb491fa94acf275e11533ca9d8a3a0
nyanmaru1026 <nyanmaru2701@gmail.com>
2020-05-14 13:15:58 +0900

    更新実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root to: 'tweets#index'
-  resources :tweets, only: [:index, :new, :create, :show, :destroy, :edit]
+  resources :tweets, only: [:index, :new, :create, :show, :destroy, :edit, :update]
 end
