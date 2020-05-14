a1daaeaa1f284834119383612bc33b2fc3a83fd9
nyanmaru1026 <nyanmaru2701@gmail.com>
2020-05-14 12:44:14 +0900

    deleteアクションの実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -15,6 +15,11 @@ class TweetsController < ApplicationController
     Tweet.create(tweet_params)
   end
 
+  def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy
+  end
+
   private
   def tweet_params
     params.require(:tweet).permit(:name, :image, :text)
