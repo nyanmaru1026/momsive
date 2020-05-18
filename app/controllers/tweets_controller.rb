0a3e4c9269c8df49282748e6982ac8051feb7ca8
nyanmaru1026 <nyanmaru2701@gmail.com>
2020-05-18 20:32:33 +0900

    コメント機能実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -10,6 +10,8 @@ class TweetsController < ApplicationController
 
   def show
     @tweet = Tweet.find(params[:id])
+    @comment = Comment.new
+    @comments = @tweet.comments.includes(:user)
   end
 
   def create
